0d1c339c0fe8579dbecef0430514ee6d2d2367cd
Miys <supergirlmiya@gmail.com>
2023-08-15 18:02:47 +0100

    Initial Commit

---
+++ ayo/ayoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ayoApp.swift
+//  ayo
+//
+//  Created by Miya  on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ayoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
